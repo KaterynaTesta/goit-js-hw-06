c3cc878b211745a76901551e4a0813f3b77ce0f4
KaterynaTesta <84970518+KaterynaTesta@users.noreply.github.com>
2021-10-17 19:30:00 +0400

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,15 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
 const ingredientEl = document.querySelector('#ingredients');
-const listofIngredients = ingredients.forEach(ingredient => {
+const listofIngredients = ingredients.map(ingredient => {
     const newItem = document.createElement('li');
     newItem.classList.add('.item');
     console.log(newItem);
     newItem.textContent = ingredient;
     console.log(newItem.textContent);
-    ingredientEl.appendChild(newItem);
+    return newItem;
 });
+
+ingredientEl.append(...listofIngredients);
+
 console.log(ingredients);
